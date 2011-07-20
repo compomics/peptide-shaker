324e46b96fd2fe72e90edd1417845fff80432974
mvaudel <mvaudel@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2011-07-20 15:55:49 +0000

    Added Javadoc and made the PTM scores serializable.

--- trunk/src/main/java/eu/isas/peptideshaker/myparameters/PSPtmScores.java
+++ trunk/src/main/java/eu/isas/peptideshaker/myparameters/PSPtmScores.java
@@ -10,30 +10,55 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
+ * This class contains the scores for the locations of the possible modifications.
  *
- * @author vaudel
+ * @author Marc
  */
 public class PSPtmScores implements UrParameter {
     
-
+    /**
+     * A map containing all scores indexed by the modification of interest
+     */
     private HashMap<String, PtmScoring> ptmMap = new HashMap<String, PtmScoring>();
     
+    /**
+     * Constructor
+     */
     public PSPtmScores() {
         
     }
     
+    /**
+     * Adds a scoring result for the modification of interest
+     * @param ptmName       the modification of interest
+     * @param ptmScoring    the corresponding scoring
+     */
     public void addPtmScoring(String ptmName, PtmScoring ptmScoring) {
         ptmMap.put(ptmName, ptmScoring);
     }
     
+    /**
+     * Returns the ptm scoring for the desired modification (null if none found).
+     * @param ptmName   the modification of interest
+     * @return the scoring
+     */
     public PtmScoring getPtmScoring(String ptmName) {
         return ptmMap.get(ptmName);
     }
     
+    /**
+     * indicates whether a modification has been already scored
+     * @param ptmName the modification of interest
+     * @return  a boolean indicating whether the modification is in the map
+     */
     public boolean containsPtm(String ptmName) {
         return ptmMap.containsKey(ptmName);
     }
     
+    /**
+     * Returns a list of scored modifications
+     * @return a list of scored modifications
+     */
     public ArrayList<String> getScoredPTMs() {
         return new ArrayList<String>(ptmMap.keySet());
     }
