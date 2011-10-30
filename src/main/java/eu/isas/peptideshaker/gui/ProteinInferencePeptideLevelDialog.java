80d4e1d96e8fcdabb25847d1566efba598e3f841
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2011-10-30 02:04:50 +0000

    The modified residues are now indicated by a star above the given residue in the sequence fragmentation plot. Fixed a bug in the ProteinInferencePeptideLevelDialog where one of the tables was editable, and thus the UniProt link didn't work. Updated utilities to version 3.2.24.

--- src/main/java/eu/isas/peptideshaker/gui/ProteinInferencePeptideLevelDialog.java
+++ src/main/java/eu/isas/peptideshaker/gui/ProteinInferencePeptideLevelDialog.java
@@ -298,9 +298,16 @@ public class ProteinInferencePeptideLevelDialog extends javax.swing.JDialog {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         retainedProteinJTable.addMouseListener(new java.awt.event.MouseAdapter() {
