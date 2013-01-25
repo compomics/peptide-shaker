3e82cc1d43d4caf3c9fd0f7fe9c5f8413423f4b9
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2013-01-25 19:32:54 +0000

    Extended the validateModifications method in IdFilter to kick out peptides with more ptms than possible ptm sites. (Mainly an X!Tandem issue.) Added new export options to the Fraction tab in the Features export dialog. Updated the SearchGUI slide of the GettingStarted dialog.

--- src/main/java/eu/isas/peptideshaker/fileimport/IdFilter.java
+++ src/main/java/eu/isas/peptideshaker/fileimport/IdFilter.java
@@ -1,5 +1,6 @@
 package eu.isas.peptideshaker.fileimport;
 
+import com.compomics.util.experiment.biology.PTM;
 import com.compomics.util.experiment.biology.PTMFactory;
 import com.compomics.util.experiment.biology.Peptide;
 import com.compomics.util.experiment.identification.Advocate;
@@ -11,6 +12,7 @@ import com.compomics.util.experiment.massspectrometry.SpectrumFactory;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import uk.ac.ebi.jmzml.xml.io.MzMLUnmarshallerException;
 
 /**
@@ -96,10 +98,11 @@ public class IdFilter implements Serializable {
         this.isPpm = isPpm;
         this.unknownPtm = unknownPTM;
     }
-    
+
     /**
-     * Validates the peptide assumption based on the peptide length and maximal e-values allowed.
-     * 
+     * Validates the peptide assumption based on the peptide length and maximal
+     * e-values allowed.
+     *
      * @param assumption the assumption to validate
      * @return a boolean indicating whether the assumption passed the test
      */
@@ -136,15 +139,16 @@ public class IdFilter implements Serializable {
         }
         return true;
     }
-    
+
     /**
      * Validates the modifications of a peptide.
-     * 
+     *
      * @param peptide the peptide of interest
      * @return a boolean indicating whether the peptide passed the test
      */
     public boolean validateModifications(Peptide peptide) {
 
+        // check it it's an unknown peptide
         if (unknownPtm) {
             ArrayList<ModificationMatch> modificationMatches = peptide.getModificationMatches();
             for (ModificationMatch modMatch : modificationMatches) {
@@ -153,6 +157,40 @@ public class IdFilter implements Serializable {
                 }
             }
         }
+
+        PTMFactory ptmFactory = PTMFactory.getInstance();
+
+        // check if there are more ptms than ptm sites
+        HashMap<String, ArrayList<ModificationMatch>> modMatches = new HashMap<String, ArrayList<ModificationMatch>>();
+        for (ModificationMatch modMatch : peptide.getModificationMatches()) {
+            if (modMatch.isVariable()) {
+                String modName = modMatch.getTheoreticPtm();
+                PTM ptm = ptmFactory.getPTM(modName);
+                if (ptm.getType() == PTM.MODAA) {
+                    if (!modMatches.containsKey(modName)) {
+                        modMatches.put(modName, new ArrayList<ModificationMatch>());
+                    }
+                    modMatches.get(modName).add(modMatch);
+                }
+            }
+        }
+
+        ArrayList<ModificationMatch> modificationMatches = peptide.getModificationMatches();
+
+        for (String modName : modMatches.keySet()) {
+            for (ModificationMatch modMatch : modificationMatches) {
+                try {
+                    ArrayList<Integer> possiblePositions = peptide.getPotentialModificationSites(ptmFactory.getPTM(modMatch.getTheoreticPtm()));
+                    if (possiblePositions.size() < modMatches.get(modName).size()) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
@@ -160,9 +198,9 @@ public class IdFilter implements Serializable {
      * Validates the mass deviation of a peptide assumption.
      *
      * @param assumption the considered peptide assumption
-     * @param spectrumKey the key of the spectrum used to get the precursor
-     * the precursor should be accessible via the spectrum factory
-     * 
+     * @param spectrumKey the key of the spectrum used to get the precursor the
+     * precursor should be accessible via the spectrum factory
+     *
      * @return a boolean indicating whether the given assumption passes the
      * filter
      * @throws IOException
