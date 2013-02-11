bc262c09ba5fe98b05bb93bc751e3bb71aaccc8d
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2013-02-11 17:03:13 +0000

    Forgot to add the new/moved ImageIconPanel class.

---
+++ trunk/src/main/java/eu/isas/peptideshaker/gui/ImageIconPanel.java
@@ -0,0 +1,33 @@
+package eu.isas.peptideshaker.gui;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ * This class extends JPanel to be able to display an ImageIcon object inside a
+ * JPanel.
+ * 
+ * @author Harald Barsnes
+ */
+public class ImageIconPanel extends JPanel {
+
+    /**
+     * The image icon to display.
+     */
+    private ImageIcon imageIcon;
+
+    /**
+     * Create a new ImageIconPanel.
+     *
+     * @param imageIcon the image icon to display
+     */
+    public ImageIconPanel(ImageIcon imageIcon) {
+        this.imageIcon = imageIcon;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(imageIcon.getImage(), 0, 0, imageIcon.getIconWidth(), imageIcon.getIconHeight(), null);
+    }
+}
