e57291a7ed7585764ea70031f8c4038c1f2f4553
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2013-12-02 16:17:17 +0000

    Minor JavaDoc fix.

--- trunk/src/main/java/eu/isas/peptideshaker/PeptideShaker.java
+++ trunk/src/main/java/eu/isas/peptideshaker/PeptideShaker.java
@@ -46,7 +46,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Set;
 import uk.ac.ebi.jmzml.xml.io.MzMLUnmarshallerException;
 
 /**
