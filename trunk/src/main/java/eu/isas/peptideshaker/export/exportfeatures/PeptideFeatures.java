6c5fd0fe5b8febc9dcc187ed17547d242bc15b07
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2014-02-03 20:00:01 +0000

    Updated utilities to version 3.21.22, fixing bugs in the auto update code for unix.

--- trunk/src/main/java/eu/isas/peptideshaker/export/exportfeatures/PeptideFeatures.java
+++ trunk/src/main/java/eu/isas/peptideshaker/export/exportfeatures/PeptideFeatures.java
@@ -29,7 +29,7 @@ public enum PeptideFeatures implements ExportFeature {
     score("Score", "Score of the peptide."),
     confidence("Confidence", "Confidence in percent associated to the peptide."),
     decoy("Decoy", "Indicates whether the peptide is a decoy (1: yes, 0: no)."),
-    validated("Validation", "Indicates whether the validation level of the protein group."),
+    validated("Validation", "Indicates the validation level of the protein group."),
     starred("Starred", "Indicates whether the match was starred in the interface (1: yes, 0: no)."),
     hidden("Hidden", "Indicates whether the match was hidden in the interface (1: yes, 0: no).");
     /**
