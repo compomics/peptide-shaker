c9fcbf665298ecdbae2baf3542b0cb303e75111b
harald.barsnes <harald.barsnes@b250ec38-03cf-dfc4-ed42-3f280861ea2e>
2014-05-22 15:05:27 +0000

    Added support for downloading individual files to the ReShake. Changed the PRIDE archive web service from wwwdev to www. Updated web-service-model to version 0.1.

--- trunk/src/main/java/eu/isas/peptideshaker/gui/pride/PrideWsTest.java
+++ trunk/src/main/java/eu/isas/peptideshaker/gui/pride/PrideWsTest.java
@@ -1,7 +1,14 @@
 package eu.isas.peptideshaker.gui.pride;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.pride.archive.web.service.model.assay.AssayDetail;
@@ -21,7 +28,7 @@ public class PrideWsTest {
     /**
      * The web service URL.
      */
-    private static final String projectServiceURL = "http://wwwdev.ebi.ac.uk/pride/ws/archive/";
+    private static final String projectServiceURL = "http://www.ebi.ac.uk/pride/ws/archive/";
 
     /**
      * Main class. For testing only.
@@ -115,58 +122,58 @@ public class PrideWsTest {
         // web service links:
         //
         // to get the number of projects
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/project/count
+        // http://www.ebi.ac.uk:80/pride/ws/archive/project/count
         //
         // to get the list of projects
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/project/list?show=1000&page=1&sort=publication_date&order=desc
+        // http://www.ebi.ac.uk:80/pride/ws/archive/project/list?show=1000&page=1&sort=publication_date&order=desc
         //
         // to get the details for a given project
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/project/PXD000375
+        // http://www.ebi.ac.uk:80/pride/ws/archive/project/PXD000375
         //
         // to get all assays for a given project
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/assay/list/project/PXD000375
+        // http://www.ebi.ac.uk:80/pride/ws/archive/assay/list/project/PXD000375
         //
         // to get the details of a given assay
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/assay/30323
+        // http://www.ebi.ac.uk:80/pride/ws/archive/assay/30323
         //
         // to get all the files for a given project
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/file/list/project/PXD000375
+        // http://www.ebi.ac.uk:80/pride/ws/archive/file/list/project/PXD000375
         //
         // to get all the files for a given assay
-        // http://wwwdev.ebi.ac.uk:80/pride/ws/archive/file/list/assay/30323
+        // http://www.ebi.ac.uk:80/pride/ws/archive/file/list/assay/30323
         //
         // to access private data:
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//        String userName = "username"; // has to be an e-mail address, for reviewers add "@ebi.ac.uk"
-//        String passWord = "password";
-//        String authString = userName + ":" + passWord;
-//        byte[] encodedAuthorisation = Base64.encodeBase64(authString.getBytes());
-//        headers.add("Authorization", "Basic " + new String(encodedAuthorisation));
-//        HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
-//
-//        String projectAccession = "PXD000651";
-//        RestTemplate template = new RestTemplate();
-//        ResponseEntity<ProjectDetail> entity;
-//        entity = template.exchange("http://wwwdev.ebi.ac.uk/pride/ws/archive/project/" + projectAccession, HttpMethod.GET, requestEntity, ProjectDetail.class);
-//
-//        if (entity == null) {
-//            System.out.println("ERROR: null return");
-//            return;
-//        }
-//
-//        if (entity.getHeaders() != null && entity.getHeaders().getLocation() != null) {
-//            String path = entity.getHeaders().getLocation().getPath();
-//            System.out.println("Path: " + path);
-//        }
-//
-//        if (entity.getStatusCode() != null) {
-//            System.out.println("Equals? " + HttpStatus.OK + " = " + entity.getStatusCode());
-//        }
-//        ProjectDetail project = entity.getBody();
-//
-//        System.out.println("The Project acc is: " + project.getAccession());
-//        System.out.println("Project desc: " + project.getProjectDescription());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        String userName = "username"; // has to be an e-mail address, for reviewers add "@ebi.ac.uk"
+        String passWord = "password";
+        String authString = userName + ":" + passWord;
+        byte[] encodedAuthorisation = Base64.encodeBase64(authString.getBytes());
+        headers.add("Authorization", "Basic " + new String(encodedAuthorisation));
+        HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
+
+        projectAccession = "PXD000651";
+        template = new RestTemplate();
+        ResponseEntity<ProjectDetail> entity;
+        entity = template.exchange("http://www.ebi.ac.uk/pride/ws/archive/project/" + projectAccession, HttpMethod.GET, requestEntity, ProjectDetail.class);
+
+        if (entity == null) {
+            System.out.println("ERROR: null return");
+            return;
+        }
+
+        if (entity.getHeaders() != null && entity.getHeaders().getLocation() != null) {
+            String path = entity.getHeaders().getLocation().getPath();
+            System.out.println("Path: " + path);
+        }
+
+        if (entity.getStatusCode() != null) {
+            System.out.println("Equals? " + HttpStatus.OK + " = " + entity.getStatusCode());
+        }
+        ProjectDetail project = entity.getBody();
+
+        System.out.println("The Project acc is: " + project.getAccession());
+        System.out.println("Project desc: " + project.getProjectDescription());
     }
 }
