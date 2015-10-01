2fc2ee2266cf752bf2b22ee32c38b206c50b99dd
Harald Barsnes <harald.barsnes@gmail.com>
2015-10-01 12:47:34 +0200

    Added the new CV term for Andromeda res files to the mzid export. (Currently not used, as we need to wait for support in Skyline.)

--- src/main/java/eu/isas/peptideshaker/export/MzIdentMLExport.java
+++ src/main/java/eu/isas/peptideshaker/export/MzIdentMLExport.java
@@ -1717,7 +1717,7 @@ public class MzIdentMLExport {
                 } else if (advocateIndex == Advocate.tide.getIndex()) {
                     writeCvTerm(new CvTerm("PSI-MS", "MS:1000914", "tab delimited text format", null));
                 } else if (advocateIndex == Advocate.andromeda.getIndex()) {
-                    // writeCvTerm(new CvTerm("PSI-MS", "MS:???", "Andromeda res file output format.", null)); //  @TODO: create and add new cv term?
+                    // writeCvTerm(new CvTerm("PSI-MS", "MS:1002576", "Andromeda result file", null)); // @TODO: include when properly added to ontology and to Skyline
                 } else {
                     // no cv term available for the given advocate...
                 }
