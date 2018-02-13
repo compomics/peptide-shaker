3a86d1ffec13f1bc2e77137be8458213877e898f
mvaudel <mvaudel@gmail.com>
2018-02-13 22:17:39 +0100

    Updated the peptide fraction table model.

--- src/main/java/eu/isas/peptideshaker/gui/tablemodels/PeptideFractionTableModel.java
+++ src/main/java/eu/isas/peptideshaker/gui/tablemodels/PeptideFractionTableModel.java
@@ -2,8 +2,8 @@ package eu.isas.peptideshaker.gui.tablemodels;
 
 import com.compomics.util.experiment.identification.Identification;
 import com.compomics.util.experiment.identification.matches.PeptideMatch;
-import eu.isas.peptideshaker.gui.PeptideShakerGUI;
 import eu.isas.peptideshaker.parameters.PSParameter;
+import eu.isas.peptideshaker.utils.DisplayFeaturesGenerator;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
@@ -17,71 +17,49 @@ import javax.swing.table.DefaultTableModel;
 public class PeptideFractionTableModel extends DefaultTableModel {
 
     /**
-     * The main GUI class.
-     */
-    private PeptideShakerGUI peptideShakerGUI;
-    /**
      * The identification of this project.
      */
-    private Identification identification;
+    private final Identification identification;
+    /**
+     * The display features generator.
+     */
+    private final DisplayFeaturesGenerator displayFeaturesGenerator;
     /**
      * A list of ordered peptide keys.
      */
-    private ArrayList<String> peptideKeys = null;
+    private ArrayList<Long> peptideKeys = null;
     /**
      * A list of ordered file names.
      */
     private ArrayList<String> fileNames = new ArrayList<>();
-    /**
-     * Set to true as soon as the real model is initiated. False means that only 
-     * the dummy constructor has been used.
-     */
-    private boolean modelInitiated = false;
 
     /**
      * Constructor which sets a new table.
      *
-     * @param peptideShakerGUI instance of the main GUI class
+     * @param identification the identification object
+     * @param displayFeaturesGenerator the display features generator
      * @param peptideKeys the peptide keys
+     * @param fileNames a list of ordered file names
      */
-    public PeptideFractionTableModel(PeptideShakerGUI peptideShakerGUI, ArrayList<String> peptideKeys) {
-        setUpTableModel(peptideShakerGUI, peptideKeys);
-        modelInitiated = true;
-    }
-    
-    /**
-     * Constructor which sets a new empty table.
-     */
-    public PeptideFractionTableModel() {
+    public PeptideFractionTableModel(Identification identification, DisplayFeaturesGenerator displayFeaturesGenerator, ArrayList<Long> peptideKeys, ArrayList<String> fileNames) {
+
+        this.identification = identification;
+        this.displayFeaturesGenerator = displayFeaturesGenerator;
+        this.peptideKeys = peptideKeys;
+        this.fileNames = fileNames;
+
     }
 
     /**
      * Update the data in the table model without having to reset the whole
      * table model. This keeps the sorting order of the table.
      *
-     * @param peptideShakerGUI the PeptideShakerGUI parent
      * @param peptideKeys the peptide keys
      */
-    public void updateDataModel(PeptideShakerGUI peptideShakerGUI, ArrayList<String> peptideKeys) {
-        setUpTableModel(peptideShakerGUI, peptideKeys);
-    }
+    public void updateDataModel(ArrayList<Long> peptideKeys) {
 
-    /**
-     * Set up the table model.
-     *
-     * @param peptideShakerGUI the PeptideShakerGUI parent
-     * @param peptideKeys the peptide keys
-     */
-    private void setUpTableModel(PeptideShakerGUI peptideShakerGUI, ArrayList<String> peptideKeys) {
-        this.peptideShakerGUI = peptideShakerGUI;
-        identification = peptideShakerGUI.getIdentification();
         this.peptideKeys = peptideKeys;
 
-        fileNames = new ArrayList<>();
-        
-        for (String fileName : identification.getSpectrumFiles()) {
-            fileNames.add(fileName);
-        }
     }
 
     /**
@@ -93,69 +71,96 @@ public class PeptideFractionTableModel extends DefaultTableModel {
 
     @Override
     public int getRowCount() {
-        if (peptideKeys != null) {
-            return peptideKeys.size();
-        } else {
-            return 0;
-        }
+
+        return peptideKeys == null ? 0 : peptideKeys.size();
+
     }
 
     @Override
     public int getColumnCount() {
+
         return fileNames.size() + 4;
+
     }
 
     @Override
     public String getColumnName(int column) {
+
         if (column == 0) {
+
             return " ";
+
         } else if (column == 1) {
+
             return "Sequence";
+
         } else if (column > 1 && column - 2 < fileNames.size()) {
+
             return fileNames.get(column - 2);
+
         } else if (column == fileNames.size() + 2) {
+
             return "Confidence";
+
         } else if (column == fileNames.size() + 3) {
+
             return "";
+
         } else {
+
             return "";
+
         }
     }
 
     @Override
     public Object getValueAt(int row, int column) {
 
-        try {
-            String peptideKey = peptideKeys.get(row);
-            PeptideMatch peptideMatch = (PeptideMatch)peptideShakerGUI.getIdentification().retrieveObject(peptideKey);
-            if (column == 0) {
-                return row + 1;
-            } else if (column == 1) {
-                return peptideShakerGUI.getDisplayFeaturesGenerator().getTaggedPeptideSequence(peptideMatch, true, true, true);
-            } else if (column > 1 && column - 2 < fileNames.size()) {
+        long peptideKey = peptideKeys.get(row);
+        PeptideMatch peptideMatch = identification.getPeptideMatch(peptideKey);
+
+        if (column == 0) {
+
+            return row + 1;
+
+        } else if (column == 1) {
+
+            return displayFeaturesGenerator.getTaggedPeptideSequence(peptideMatch, true, true, true);
+
+        } else if (column > 1) {
+            
+                PSParameter psParameter = (PSParameter) peptideMatch.getUrParam(PSParameter.dummy);
+
+            if (column - 2 < fileNames.size()) {
+
                 String fraction = fileNames.get(column - 2);
-                PSParameter pSParameter = new PSParameter();
-                pSParameter = (PSParameter)peptideMatch.getUrParam(pSParameter);
-                if (pSParameter.getFractionScore() != null && pSParameter.getFractions().contains(fraction)) {
-                    return pSParameter.getFractionConfidence(fraction);
-                } else {
-                    return 0.0;
+                
+                if (psParameter.getFractionScore() != null) {
+                    
+                    Double confidence = psParameter.getFractionConfidence(fraction);
+                    
+                    if (confidence != null) {
+                        
+                        return confidence;
+                        
+                    }
                 }
+
+                return  0.0;
+                
             } else if (column == fileNames.size() + 2) {
-                PSParameter pSParameter = new PSParameter();
-                pSParameter = (PSParameter)peptideMatch.getUrParam(pSParameter);
-                return pSParameter.getPeptideConfidence();
+
+                return psParameter.getConfidence();
+
             } else if (column == fileNames.size() + 3) {
-                PSParameter pSParameter = new PSParameter();
-                pSParameter = (PSParameter)peptideMatch.getUrParam(pSParameter);
-                return pSParameter.getMatchValidationLevel().getIndex();
-            } else {
-                return "";
+
+                return psParameter.getMatchValidationLevel().getIndex();
+
             }
-        } catch (Exception e) {
-            peptideShakerGUI.catchException(e);
-            return "";
         }
+
+        return "";
+
     }
 
     @Override
@@ -171,23 +176,5 @@ public class PeptideFractionTableModel extends DefaultTableModel {
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return false;
-    }
-    
-    /**
-     * Returns true if the real model has been initiated.
-     * 
-     * @return the modelInitiated
-     */
-    public boolean isModelInitiated() {
-        return modelInitiated;
-    }
-
-    /**
-     * Set if the real model has been initiated.
-     * 
-     * @param modelInitiated the modelInitiated to set
-     */
-    public void setModelInitiated(boolean modelInitiated) {
-        this.modelInitiated = modelInitiated;
     }
 }
