df61f204100cc131b2b6f3db8ec474299840ab08
Jakub Vasicek <j.vasicek58@gmail.com>
2023-08-04 14:32:11 +0200

    optimization of the Percolator export (enzymaticity features)

--- src/main/java/eu/isas/peptideshaker/utils/PercolatorUtils.java
+++ src/main/java/eu/isas/peptideshaker/utils/PercolatorUtils.java
@@ -380,7 +380,7 @@ public class PercolatorUtils {
                     if (locationN && locationC) {
 
                         nc = true;
-
+                        break;
                     }
 
                     if (locationN) {
@@ -394,6 +394,11 @@ public class PercolatorUtils {
                         c = true;
 
                     }
+                }
+                
+                // if the peptide is enzymatic for one protein, don't check the rest of the matching proteins
+                if (nc) {
+                    break;
                 }
             }
 
